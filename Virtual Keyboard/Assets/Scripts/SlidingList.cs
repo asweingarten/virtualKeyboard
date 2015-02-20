75e76d6edbf09d772246b180aa948fd817a66725
asweingarten <asweingarten+github@gmail.com>
2015-02-20 16:56:29 -0500

    Fixed some compilation and null reference errors. Also reversed direction of slide_list_increment for up and down arrow buttons.

--- Virtual Keyboard/Assets/Scripts/SlidingList.cs
+++ Virtual Keyboard/Assets/Scripts/SlidingList.cs
@@ -30,14 +30,14 @@ public class SlidingList : MonoBehaviour {
 		base_scale = listOfItems.parent.transform.localScale;
 
 		items = new List<InnerListItem> ();
+
+		Transform buttons = transform.Find ("Buttons");
 		
-		addButton = transform.Find ("AddButton").gameObject;
-		upArrow = transform.Find ("UpArrow").gameObject;
-		downArrow = transform.Find ("DownArrow").gameObject;
+		addButton = buttons.Find ("AddButton").gameObject;
+		upArrow = buttons.Find ("UpArrow").gameObject;
+		downArrow = buttons.Find ("DownArrow").gameObject;
 		
 		MenuItemSelection.OnMenuItemHold += HandleOnMenuItemHeld;
-
-		Debug.Log(transformList.Count);
 
 		createNewListItem ("blahblah");
 		createNewListItem ("blahblah 2");
@@ -61,11 +61,11 @@ public class SlidingList : MonoBehaviour {
 	}
 
 	void handleUpArrow() {
-		slide_list_increment(1);
+		slide_list_increment(-1);
 	}
 
 	void handleDownArrow() {
-		slide_list_increment(-1);
+		slide_list_increment(1);
 	}
 	
 	// Update is called once per frame
