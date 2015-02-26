08c46a813bb5881ac36912fb91efb29ce54c2b04
Mark Roukema <mroukema@gmail.com>
2015-02-26 17:02:50 -0500

    Forgot to add debug behaviour in last push

---
+++ Virtual Keyboard/Assets/RadialActionFireDebug.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadialActionFireDebug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ArcMeshDrawer.OnRadialMenuActionTrigger += debugMessage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void debugMessage(GameObject selected) {
+		if (selected == gameObject) {
+			Debug.Log ("Arc section selected");
+		}
+	}
+}
