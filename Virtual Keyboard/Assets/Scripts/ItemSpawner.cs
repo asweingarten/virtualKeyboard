20acd65be219d558c4206012a08d0b44bd3b5216
Adam Watson <a3watson@uwaterloo.ca>
2015-03-03 18:20:00 -0500

    Modified ItemSpawner to spawn items directly where the user is looking

--- Virtual Keyboard/Assets/Scripts/ItemSpawner.cs
+++ Virtual Keyboard/Assets/Scripts/ItemSpawner.cs
@@ -20,34 +20,39 @@ public class ItemSpawner : MonoBehaviour
 
 	}
 
+	// Send out a raycast from the camera's position. It should collide with some surface.
+	// If that surface is the surface argument, return the position of the collision (where to spawn a new object)
+	Vector3 calculateSpawnLocationOnSurface(GameObject surface) {
+		RaycastHit hit;
+		if (MainCamera != null) {
+			if (surface != null) {
+				if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out hit)) {
+					if (surface == hit.collider.gameObject) {
+						Debug.Log("CONTACT" + hit.point);
+						return hit.point;
+					}
+				}
+			}
+		}
+		return Vector3.zero;
+	}
+
 	Vector3 calculateSpawnLocation(Furniture.Mounting mounting) {
 		switch(mounting) {
 			case Furniture.Mounting.CEILING:
-				if (Ceiling == null) { return Vector3.zero; }
-				return Ceiling.transform.position;
+				return calculateSpawnLocationOnSurface(Ceiling);
 				break;
 			case Furniture.Mounting.FLOOR:
-				if (Floor == null) { return Vector3.zero; }
-				return Floor.transform.position;
+				return calculateSpawnLocationOnSurface(Floor);
 				break;
-
 			case Furniture.Mounting.WALL:
-				if (MainCamera != null) {
-					// Send out a raycast from the camera's position, if it collides with a wall, return the wall's position.
-					RaycastHit hit;
-					if(Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out hit)) {
-	    				for (int i = 0; i < 4; i++) {
-							if (Walls[i] == null) { continue; }
-							if (Walls[i].transform.position == hit.transform.position) {
-								Debug.Log("Collision at" + Walls[i].transform.position);
-								return Walls[i].transform.position;
-							}
-						}
+				Vector3 collisionSpot;
+	    		for (int i = 0; i < 4; i++) {
+					collisionSpot = calculateSpawnLocationOnSurface(Walls[i]);
+					if (collisionSpot != Vector3.zero) {
+						return collisionSpot;
 					}
 				}
-
-				Debug.Log("No collisions with walls were recorded");
-				return Vector3.zero;
 				break;
 		}
 		return Vector3.zero;
@@ -56,9 +61,11 @@ public class ItemSpawner : MonoBehaviour
 	void spawnFurniture(Furniture furniture) {
 		Debug.Log ("THE SPAWNER HAS SPOKEN");
 		Vector3 spawnLocation = calculateSpawnLocation(furniture.mounting);
-		Quaternion rotationQ = Quaternion.Euler(furniture.rotation);
-		GameObject obj = (GameObject)Instantiate(furniture.model, spawnLocation, rotationQ);
-		obj.transform.localScale = furniture.scale;
+		if (spawnLocation != Vector3.zero) {
+			Quaternion rotationQ = Quaternion.Euler(furniture.rotation);
+			GameObject obj = (GameObject)Instantiate(furniture.model, spawnLocation, rotationQ);
+			obj.transform.localScale = furniture.scale;
+		}
 	}
 
 }
