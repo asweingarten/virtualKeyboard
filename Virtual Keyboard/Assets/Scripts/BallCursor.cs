580b9aa4f1325653c9f98056e546df4ac04a7fdb
asweingarten+github@gmail.com <asweingarten+github@gmail.com>
2015-03-03 16:31:46 -0500

    Add sensitivity and noise thresholds to ball cursor
    Sensitivity determines the intervals the cursor moves in.
    Movements below the noise threshold are ignored. This enables stead
    control of the cursor.

--- Virtual Keyboard/Assets/Scripts/BallCursor.cs
+++ Virtual Keyboard/Assets/Scripts/BallCursor.cs
@@ -5,6 +5,8 @@ using Leap;
 public class BallCursor : MonoBehaviour
 {
 	Controller controller;
+	public float sensitivity = 50f;
+	public float noiseThreshold = 2f;
 	// Use this for initialization
 	void Start ()
 	{
@@ -24,9 +26,15 @@ public class BallCursor : MonoBehaviour
 	}
 
 	Vector3 calculateUnityTranslationVector(Vector vec) {
-		float movementIncrementX = vec.x != 0 && Mathf.Abs(vec.x) > 2 ? 0.001f : 0;
-		float movementIncrementY = vec.y != 0 && Mathf.Abs (vec.y) > 2 ? 0.001f : 0;
-		float movementIncrementZ = vec.z != 0 && Mathf.Abs(vec.z) > 2 ? 0.001f : 0;
+		float movementIncrementX = vec.x != 0 && Mathf.Abs(vec.x) > noiseThreshold 
+			? 0.001f*sensitivity 
+			: 0;
+		float movementIncrementY = vec.y != 0 && Mathf.Abs (vec.y) > noiseThreshold 
+			? 0.001f*sensitivity 
+			: 0;
+		float movementIncrementZ = vec.z != 0 && Mathf.Abs(vec.z) > noiseThreshold
+			? 0.001f*sensitivity 
+			: 0;
 		float xDirection = vec.x >= 0 ? 1f : -1f;
 		float yDirection = vec.y >= 0 ? 1f : -1f;
 		float zDirection = vec.z <= 0 ? 1f : -1f;
