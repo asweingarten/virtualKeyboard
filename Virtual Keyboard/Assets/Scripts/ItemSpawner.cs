f161836063f69ec9e4b6221b1882b9add34b5116
Adam Watson <a3watson@uwaterloo.ca>
2015-03-03 16:54:07 -0500

    Updated the ItemSpawner to spawn items on walls
    Updated the ItemSpawner to spawn an item on the wall being faced. Also
    updated the sliding list prefab to include a "ignore raycasting" layer.

--- Virtual Keyboard/Assets/Scripts/ItemSpawner.cs
+++ Virtual Keyboard/Assets/Scripts/ItemSpawner.cs
@@ -6,7 +6,6 @@ public class ItemSpawner : MonoBehaviour
 	public GameObject Ceiling = null;
 	public GameObject Floor = null;
 	public GameObject[] Walls = new GameObject[4];
-	
 
 	// Use this for initialization
 	void Start ()
@@ -23,16 +22,28 @@ public class ItemSpawner : MonoBehaviour
 	Vector3 calculateSpawnLocation(Furniture.Mounting mounting) {
 		switch(mounting) {
 			case Furniture.Mounting.CEILING:
-				if (Ceiling == null) { return Vector3.zero;}
+				if (Ceiling == null) { return Vector3.zero; }
 				return Ceiling.transform.position;
 				break;
 			case Furniture.Mounting.FLOOR:
-				if (Floor == null) { return Vector3.zero;}
+				if (Floor == null) { return Vector3.zero; }
 				return Floor.transform.position;
 				break;
 
 			case Furniture.Mounting.WALL:
-				//if (Ceiling == null) { return Vector3.zero;}
+				Camera camera = Camera.mainCamera;
+				RaycastHit hit;
+				// Send out a raycast from the camera's position, if it collides with a wall, return the wall's position.
+				if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit)) {
+    				for (int i = 0; i < 4; i++) {
+						if (Walls[i] == null) { continue; }
+						if (Walls[i].transform.position == hit.transform.position) {
+							Debug.Log("Collision at" + Walls[i].transform.position);
+							return Walls[i].transform.position;
+						}
+					}
+				}
+
 				return Vector3.zero;
 				break;
 		}
