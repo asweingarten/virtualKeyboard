30a003402eb308602a1f87333cec9a2c5d9bba22
Mark Roukema <mroukema@gmail.com>
2015-03-05 16:40:39 -0500

    Added circle gesture to toggle radial menu

--- Virtual Keyboard/Assets/MenuShortcuts.cs
+++ Virtual Keyboard/Assets/MenuShortcuts.cs
@@ -1,24 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap;
 
 [RequireComponent (typeof (ItemSelection))]
 public class MenuShortcuts : MonoBehaviour {
 
 	public char menuKey = 'm';
 
+	private Controller leapController;
 	private ItemSelection itemSelection;
 	public GameObject menuObject;
+
+	void Awake () {
+		leapController = new Controller ();
+	}
 
 	// Use this for initialization
 	void Start () {
 		itemSelection = GetComponent<ItemSelection> ();
+		leapController.EnableGesture (Leap.Gesture.GestureType.TYPECIRCLE);
+		//leapController.config ();
+		leapController.Config.SetFloat ("Gesture.Circle.MinRadius", 15.0f);
+		leapController.Config.SetFloat ("Gesture.Circle.MinArc", 2*Mathf.PI);
+		//Leap.Gesture.Circle.MinRadius = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log (radialTransformManager);
-		if (isSelected() && Input.GetKeyDown (""+menuKey)) {
+		if( !isSelected() ) return;
+
+		if (Input.GetKeyDown (""+menuKey)) {
 			menuObject.SetActive(!menuObject.activeSelf);
+			return;
+		}
+
+		Frame frame = leapController.Frame ();
+		GestureList gestures = frame.Gestures();
+		
+		foreach (Gesture gesture in gestures) {
+			if( gesture.State.Equals(Gesture.GestureState.STATESTOP) ) {
+				menuObject.SetActive(!menuObject.activeSelf);
+				return;
+			}
 		}
 	}
 
--- Virtual Keyboard/Assets/Scripts/ItemManipulator.cs
+++ Virtual Keyboard/Assets/Scripts/ItemManipulator.cs
@@ -23,7 +23,7 @@ public class ItemManipulator : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 		if (selectedItem == null) return;
-
+		if ( !isHandClosed() ) return;
 		//translationEnabled = isHandClosed ();
 
 		if (translationEnabled) {
