61f6a946b102794bc4b17cade48b044dba39a691
Mark Roukema <mroukema@gmail.com>
2015-03-12 18:58:30 -0400

    Added KeyboardInteractionManager for sliding list

---
+++ Virtual Keyboard/Assets/Scripts/SlidingListKeyboardInteractionManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingListKeyboardInteractionManager : SlidingListInteractionManager {
+
+	public string prevCategoryTrigger;
+	public string nextCategoryTrigger;
+	public string prevItemTrigger;
+	public string nextItemTrigger;
+	public string categoryListTrigger;
+
+	private char activeCharacter;
+
+	void Awake () {
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string inputString = Input.inputString;
+		foreach( char c in inputString ) {
+			if (inputString.Contains(prevCategoryTrigger)) {
+				activeCharacter = c;
+				prevCatagory();
+			}
+			if (inputString.Contains(nextCategoryTrigger)){
+				activeCharacter = c;
+				nextCategory();
+			}
+			if (inputString.Contains(prevItemTrigger)) {
+				activeCharacter = c;
+				prevListItem();
+			}
+			if (inputString.Contains(nextItemTrigger)) {
+				activeCharacter = c;
+				nextListItem();
+			}
+			if (inputString.Contains(categoryListTrigger)) {
+				activeCharacter = c;
+				categoryListView();
+			}
+
+		}
+
+	}	
+}
