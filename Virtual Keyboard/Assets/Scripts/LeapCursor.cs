2b61efc7842eda85848500234db18e342b0da0bd
Mark Roukema <mroukema@gmail.com>
2015-03-17 17:01:49 -0400

    PanelCursor using leap motion hand controller prefab

---
+++ Virtual Keyboard/Assets/Scripts/LeapCursor.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapCursor : HandModel
+{
+	public GameObject cursor;
+	//private const int NUM_FINGERS = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		//NUM_FINGERS = 0;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public override void InitHand ()
+	{
+		SetPositions();
+	}
+
+	public override void UpdateHand() {
+		SetPositions();
+	}
+
+	protected virtual void SetPositions() {
+		if( cursor != null ) {
+			cursor.transform.position = GetPalmPosition();
+		}
+	}
+}
+
